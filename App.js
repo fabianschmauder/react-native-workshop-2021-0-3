0012587dd056143aaeacf70a2b1913de1eeec65e
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-03-17 15:58:47 +0100

    Add safe area view to prevent drawing outside safe area

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AlbumScreen from "./src/screens/AlbumsScreen";
 import SettingsScreen from "./src/screens/SettingsScreen";
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-    return (<NavigationContainer>
-                <Tab.Navigator>
-                    <Tab.Screen name="Albums" component={AlbumScreen} />
-                    <Tab.Screen name="Settings" component={SettingsScreen} />
-                </Tab.Navigator>
-            </NavigationContainer>);
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <NavigationContainer>
+                    <Tab.Navigator>
+                        <Tab.Screen name="Albums" component={AlbumScreen}/>
+                        <Tab.Screen name="Settings" component={SettingsScreen}/>
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+        </SafeAreaProvider>);
 }
